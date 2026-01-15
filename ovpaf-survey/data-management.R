4fada0db2f5435e612796b5793502e2bb56701c9
chris-allones <christopher.allones@gmail.com>
2026-01-15 17:11:47 +0800

    faculty and staff ini results

--- ovpaf-survey/data-management.R
+++ ovpaf-survey/data-management.R
@@ -30,7 +30,7 @@ item_statement_dta <-
   mutate(total = sum(n),
          perc = n/total) |> 
   ungroup() |>
-  mutate(description = str_wrap(description, width = 60)) |> 
+  mutate(description = str_wrap(description, width = 40)) |> 
   mutate(response_fct = factor(response, 
                                levels = c(1, 2, 3, 4, 5),
                                labels = c("Strongly Disagree", 
@@ -41,20 +41,3 @@ item_statement_dta <-
   mutate(response_fct = fct_rev(response_fct))
 
 
-## plotting statement
-item_statement_dta |> 
-  filter(str_detect(categories, "Emotional")) |>
-  ggplot(aes(x = perc, y = description, fill = response_fct)) +
-  geom_col(position = "fill", width = 0.7) +
-  geom_text(aes(label = scales::percent(perc, accuracy = 1)),
-            position = position_fill(vjust = 0.5),
-            color = "white",
-            size = 5) +
-  scale_x_continuous(labels = scales::percent_format()) +
-  facet_wrap(~position) +
-  labs(title = "Perception on Safety and Resilience Statements",
-       x = "Percentage",
-       y = "Statement",
-       fill = "Response") +
-  theme_minimal() +
-  theme(legend.position = "bottom")
