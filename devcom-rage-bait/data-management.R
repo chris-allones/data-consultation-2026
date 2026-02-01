af0b5d88c80268df3a48424d431d598a0439163f
Christopher A. Llones <christopher.llones@vsu.edu.ph>
2026-02-01 09:03:10 +0800

    rage bait factors plotted

--- devcom-rage-bait/data-management.R
+++ devcom-rage-bait/data-management.R
@@ -3,8 +3,21 @@ library(tidyverse)
 library(janitor)
 library(readxl)
 
-# data management
+# custom theme
+custom_theme <- 
+  theme_minimal() +
+  theme(plot.title = element_text(hjust = 0, size = 16, margin = margin(b=15), face = "bold"),
+        plot.title.position = "plot",
+        plot.subtitle = element_text(color = "gray40", margin = margin(b=15), size = 12),
+        plot.margin = margin(t = 20, r = 20, b = 20, l = 20),
+        panel.grid = element_blank(),
+        axis.text = element_text(size = 12),
+        strip.text = element_text(size = 16, face = "bold"),
+        legend.position = "bottom",
+        legend.text = element_text(size = 12)
+      )
 
+# data management
 ## import data
 
 likert_res_dta <- 
@@ -14,6 +27,15 @@ likert_res_dta <-
 socio_demo_dta <- 
   read_excel("data/rage-bait-data.xlsx", sheet = 2) |> 
   clean_names()
+
+likert_statement_dta <- 
+  read_excel("data/rage-bait-data.xlsx", sheet = 3) |> 
+  clean_names() |> 
+  mutate(
+    statement = str_trim(statement, "both"),
+    categories = str_to_title(categories),
+    item = str_to_lower(item)
+  )
 
 
 ## by social media platform
@@ -45,6 +67,49 @@ soc_med_content_dta <-
   soc_med_category_sub_dta |> 
   left_join(soc_med_category) |> 
   select(-n)
+
+
+## likert statement proportions
+fct_response_dta <- 
+  likert_res_dta |>
+  select(rcr1:are6) |> 
+  pivot_longer(
+    cols = everything(),
+    names_to = "item",
+    values_to = "response"
+  ) |> 
+  left_join(likert_statement_dta, by = "item") |> 
+  relocate(categories, .before = item) |> 
+  mutate(response = if_else(response == "Dsiagree", "Disagree", response)) |> # correnting typo error in the dataset
+  count(categories, statement, response) |>
+  group_by(statement) |> 
+  mutate(pct = n / sum(n)) |> 
+  ungroup() |> 
+  na.omit()
   
+## creating function to plot likert responses
+plt_rage_fct <- function(category){
+  fct_response_dta |> 
+  mutate(fct_response = factor(response, levels = c("Strongly Agree", "Agree", "Neutral", "Disagree", "Strongly Disagree"))) |> 
+  mutate(
+    statement = str_wrap(statement, 50),
+    pct_label = round(pct * 100, 0)
+  ) |> 
+  filter(str_detect(categories, category)) |> 
+  ggplot(aes(pct, statement, fill = fct_response)) +
+  geom_col(width = 0.7) +
+  geom_text(aes(label = pct_label), position = position_fill(vjust = 0.5), color = "white", fontface = "bold") +
+  scale_x_continuous(labels = label_percent()) +
+  scale_fill_manual(values = c("#073b4c", "#118ab2", "#06d6a0", "#ffba66ff", "#e63946")) +
+  coord_cartesian(clip = "off") +
+  facet_wrap(~ categories) +
+  labs(
+    y = NULL,
+    x = NULL,
+    fill = NULL
+  ) +
+  custom_theme
+}
+
 
 
