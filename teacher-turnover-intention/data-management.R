6b62a2a73eff6bc8f2039a778dbfa4229de51a84
Christopher A. Llones <christopher.llones@vsu.edu.ph>
2026-04-29 06:39:16 +0800

    data clean socio variables

--- teacher-turnover-intention/data-management.R
+++ teacher-turnover-intention/data-management.R
@@ -1,12 +1,4 @@
-# loading data
-df <- read_excel("data/turnover-data.xlsx") |>
-  clean_names() |>
-  mutate(
-    age = str_extract(age, "\\d+"),
-    age = as.numeric(age),
-    weekly_teaching_hours = str_extract(weekly_teaching_hours, "\\d+"),
-    weekly_teaching_hours = as.numeric(weekly_teaching_hours)
-  )
+# default theme setting
 
 # custom theme
 custom_theme <-
@@ -32,22 +24,71 @@ custom_theme <-
     legend.text = element_text(size = 12)
   )
 
-## data cleaning
-df |> glimpse()
-df |> count(highest_educational_attainment) |> View()
 
-df |>
+
+# loading data
+## cleaning data
+df <- read_excel("data/turnover-data.xlsx") |>
+  clean_names() |>
   mutate(
+    age = str_extract(age, "\\d+"),
+    age = as.numeric(age),
+    weekly_teaching_hours = str_extract(weekly_teaching_hours, "\\d+"),
+    weekly_teaching_hours = as.numeric(weekly_teaching_hours),
     highest_educational_attainment = case_when(
-      str_detect(highest_educational_attainment, "College") ~ "College degree",
-      str_detect(
-        highest_educational_attainment,
-        "Associate"
-      ) ~ "Associate course",
-      TRUE ~ highest_educational_attainment
-    )
-  ) |>
-  count(highest_educational_attainment)
+                                    str_detect(highest_educational_attainment, "College") ~ "College degree",
+                                    str_detect( highest_educational_attainment, "Associate") ~ "Associate course",
+                                    TRUE ~ highest_educational_attainment),
+    num = as.numeric(str_extract(experience, "\\d+")),    
+    unit = case_when(
+      str_detect(str_to_lower(experience), "month|months") ~ "month",
+      str_detect(str_to_lower(experience), "yr|year|years|year's") ~ "year",
+      TRUE ~ "year"),
+    experience_years = case_when(
+      unit == "month" ~ num / 12,
+      unit == "year"  ~ num,
+      TRUE ~ num),
+    experience_years = if_else(experience_years < 0.1, 21/12, experience_years)
+  ) |> 
+  mutate(
+    # Lowercase for consistency
+    monthly_income = str_to_lower(monthly_income),
+    # Remove peso signs, commas, "p", "php", "₱", "pesos", "usd", words
+    monthly_income = str_replace_all(monthly_income, "₱|php|p|pesos|usd", ""),
+    monthly_income = str_replace_all(monthly_income, "[^0-9k\\- ]", ""),
+    monthly_income = str_trim(monthly_income),
+    monthly_income = str_replace_all(monthly_income, "(\\d+)\\s*k", "\\1000"),
+    income_value = case_when(
+      str_detect(monthly_income, "-") ~ {
+        nums <- str_extract_all(monthly_income, "\\d+")[[1]]
+        mean(as.numeric(nums))
+      },
+      TRUE ~ as.numeric(str_extract(monthly_income, "\\d+"))
+    ),
+    income_value = if_else(income_value <= 30, income_value * 1000, income_value)
+  ) |> 
+  select(-num, -unit, -experience, -monthly_income) |> 
+  relocate(experience_years, .after = highest_educational_attainment) |> 
+  relocate(income_value, .after = esl_platform)
+
+## data for ESL platform used
+dfdf_els_platform <- df |>
+  separate_rows(esl_platform, sep = ",|&|and|/") |>
+  mutate(esl_platform = str_trim(esl_platform)) |>
+  mutate(esl_platform = str_to_lower(esl_platform)) |>
+  mutate(esl_platform = case_when(
+    str_detect(esl_platform, "team") ~ "teams",
+    str_detect(esl_platform, "class") ~ "classin",
+    str_detect(esl_platform, "zoom") ~ "zoom",
+    str_detect(esl_platform, "voov") ~ "voov",
+    str_detect(esl_platform, "gmeet|google meet") ~ "google meet",
+    str_detect(esl_platform, "skype") ~ "skype",
+    str_detect(esl_platform, "palfish") ~ "palfish",
+    str_detect(esl_platform, "qq") ~ "qq",
+    str_detect(esl_platform, "company") ~ "company platform",
+    TRUE ~ esl_platform
+  )) |> 
+  select(esl_platform)
 
 
 ## likert items data
