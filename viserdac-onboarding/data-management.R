be52345bbbd811161c040e049d16fc6bb334de75
chris-allones <christopher.allones@gmail.com>
2026-08-11 13:37:21 +0800

    cloud plot training

--- viserdac-onboarding/data-management.R
+++ viserdac-onboarding/data-management.R
@@ -6,10 +6,12 @@ library(tidyverse)
 library(readxl)
 library(janitor)
 library(lubridate)
+library(tidytext)
+library(ggwordcloud)
 
 ## import data
-viserdac_dta <- 
-  read_excel("viserdac-data.xlsx") |> 
+viserdac_dta <-
+  read_excel("viserdac-data.xlsx") |>
   clean_names()
 
 
@@ -40,7 +42,10 @@ viserdac_dta <- viserdac_dta |>
 ### line plot
 viserdac_dta |>
   mutate(year_clean = year(date_clean)) |>
-  summarise(total_participants = sum(participant, na.rm = TRUE), .by = year_clean) |>
+  summarise(
+    total_participants = sum(participant, na.rm = TRUE),
+    .by = year_clean
+  ) |>
   arrange(year_clean) |>
   mutate(cumulative_participants = cumsum(total_participants)) |>
   ggplot(aes(x = year_clean, y = cumulative_participants)) +
@@ -53,18 +58,115 @@ viserdac_dta |>
   )
 
 ## bar plot
-viserdac_dta |>
+p_cumm_trained_people <- viserdac_dta |>
   mutate(year_clean = year(date_clean)) |>
-  summarise(total_participants = sum(participant, na.rm = TRUE), .by = year_clean) |>
+  summarise(
+    total_participants = sum(participant, na.rm = TRUE),
+    .by = year_clean
+  ) |>
   arrange(year_clean) |>
   mutate(cumulative_participants = cumsum(total_participants)) |>
   ggplot(aes(x = year_clean, y = cumulative_participants)) +
-  geom_col() +
-  geom_text(aes(label = cumulative_participants), vjust = -0.5, size = 3) +
+  geom_col(fill = "#e86060") +
+  geom_text(
+    aes(label = cumulative_participants, y = cumulative_participants + 250),
+    size = 5,
+    fontface = "bold"
+  ) +
+  geom_line(
+    aes(y = cumulative_participants + 100),
+    color = "steelblue",
+    linewidth = 1
+  ) +
+  geom_point(
+    aes(y = cumulative_participants + 100),
+    color = "steelblue",
+    size = 2
+  ) +
+  scale_x_continuous(breaks = seq(2017, 2026, by = 1)) +
+  scale_y_continuous(breaks = seq(0, 5e3, by = 1e3), limits = c(0, 5e3)) +
   labs(
-    x = "Year",
-    y = "Cumulative participants",
+    x = NULL,
+    y = NULL,
     title = "Cumulative training participants over time"
+  ) +
+  theme_minimal() +
+  theme(
+    plot.margin = margin(1, 1, 1, 1, "cm"),
+    axis.text = element_text(size = 12, face = "bold"),
+    plot.title = element_text(
+      size = 18,
+      face = "bold",
+      hjust = 0.5,
+      margin = margin(b = 20)
+    ),
+    axis.title = element_text(size = 14),
+    panel.grid.minor.x = element_blank(),
   )
 
+# saving the plot
+ggsave(
+  filename = "plot/cumulative_training_participants.png",
+  plot = p_cumm_trained_people,
+  width = 10,
+  height = 6,
+  dpi = 300
+)
 
+
+## wordcloud of training topics
+
+word_groups <- c(
+  "statistical" = "statistics",
+  "economy" = "economic",
+  "economics" = "economic",
+  "methods" = "method",
+  "projects" = "project",
+  "regional" = "region",
+  "regions" = "region",
+  "agricultural" = "agriculture",
+  "agribusiness" = "agriculture"
+)
+
+exclude_words <- c(
+  "training",
+  "workshop",
+  "webinar",
+  "analysis",
+  "spss",
+  "series",
+  "seminar",
+  "lecture",
+  "assessment",
+  "evaluation",
+  "project",
+  "projects"
+)
+
+training_dec_dta <-
+  viserdac_dta |>
+  select(training) |>
+  unnest_tokens(word, training) |>
+  anti_join(stop_words, by = "word") |>
+  mutate(word = recode(word, !!!word_groups)) |>
+  filter(!word %in% exclude_words) |>
+  count(word, sort = TRUE) |>
+  filter(n > 1)
+
+p_final <- ggplot(training_dec_dta, aes(label = word, size = n)) +
+  geom_text_wordcloud(rm_outside = TRUE, color = "#e86060") +
+  scale_size_area(max_size = 20) +
+  theme_minimal()
+
+print(p_final)
+
+
+## save wordcloud plot
+ggsave(
+  filename = "plot/training_wordcloud.png",
+  plot = p_final,
+  width = 10,
+  height = 8,
+  dpi = 300,
+  bg = "white"
+)
