ef02f34befb7d5f8120da81ddce42fd8dd2b0a91
Christopher A. Llones <christopher.llones@vsu.edu.ph>
2026-09-12 23:19:34 +0800

    interpretation self efficacy

--- rentaro-ai-effects-academic/data-management.R
+++ rentaro-ai-effects-academic/data-management.R
@@ -327,3 +327,13 @@ sem_ai_ms_dta <- ai_ms_dta |>
     )
   )) |>
   select(a1:ai_use4)
+
+
+lkrt_dta |>
+  mutate(statement = str_wrap(statement, width = 40)) |>
+  filter(str_detect(
+    factor,
+    fixed("Effort Expectancy\n(Self-Efficacy)")
+  )) |>
+  select(-factor) |>
+  print(n = 50)
